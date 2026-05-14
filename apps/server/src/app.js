f82d2b1cf91ace1e47cd2cc9eecc11c18c49b2d3
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2026-05-14 10:23:27 +0530

    Bizflow ai crm development setup complete

---
+++ apps/server/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import prisma from "./prisma/client";
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(cookieParser());
+app.get("/test-db", async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+});
+app.get("/", (req, res) => {
+    res.json({
+        message: "BizFlow AI CRM API Running",
+    });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+//# sourceMappingURL=app.js.map
